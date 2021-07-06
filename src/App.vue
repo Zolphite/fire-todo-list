7363cb045d36371c0523e8e8a28d42a9a2687474
Zolphite <qureshia211217@gmail.com>
2021-07-05 21:34:36 -0500

    Added top nav bar with title and logo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <topBar></topBar>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import topBar from './components/top-bar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    topBar
   }
 }
 </script>
---
+++ src/components/top-bar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="top-bar">
+            <h1 class="title"><i class="fas fa-tasks logo"></i>Fire TODO List</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .top-bar{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background: rgb(12, 105, 141);
+        color: white;
+    }
+    .title{
+        float: center;
+    }
+    .logo{
+        float: center;
+        margin-right: 1rem;
+        margin-left: 1rem;
+    }
+</style>
