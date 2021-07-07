280e9de12dd01cdaee17ac891447a5e164d46fdf
Zolphite <qureshia211217@gmail.com>
2021-07-07 01:54:27 -0500

    added simple table that displays list data and action buttons | added create new task button

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="todo-list pt-2">
-    <h1 class="py-3">Todo List</h1>
+    <h3 class="py-3">
+      Todo List
+    </h3>
+    <div class="btn btn-outline-primary float-end mx-5 mb-2">Create New Task</div>
+    <table id="todo-list-table" ref="todo" class="table table-striped" style="width:100%">
+        <thead>
+          <tr>
+            <th class="h4">Items</th>
+            <th class="h4">Times</th>
+            <th class="h4">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in todoListItems" :key="item">
+            <th class="todo-item px-5">{{item.desc}}</th>
+            <th>{{item.time}}</th>
+            <th class="action-item">
+              <div class="btn btn-outline-success mx-2">Edit</div>
+              <div class="btn btn-outline-danger mx-2">Delete</div>
+            </th>
+          </tr>
+        </tbody>
+    </table>
+    <div class="btm-padding">
+    </div>
   </div>
 </template>
 
@@ -16,12 +40,39 @@ export default {
         1: {
           desc: 'Get water',
           time: '2013'
-        }
+        },
+        2: {
+          desc: 'Get water',
+          time: '2013'
+        },
+        3: {
+          desc: 'Find watch',
+          time: '2015'
+        },
+        4: {
+          desc: 'Get water',
+          time: '2013'
+        },
+        5: {
+          desc: 'Get water',
+          time: '2013'
+        },
+        6: {
+          desc: 'Get water',
+          time: '2013'
+        },
+        7: {
+          desc: 'Get water',
+          time: '2013'
+        },
       },
     }
   },
   mounted(){
-
+    // this.$nextTick(funciton () {
+    //   console.log(this.$refs.myid)
+    // });
+    // document.getElementById('todo-list-table').DataTable();
   },
   methods: {
 
@@ -30,5 +81,12 @@ export default {
 </script>
 
 <style>
+.todo-item{
+  width: 55%;
+  text-align: left;
+}
 
+.btm-padding {
+  height: 100px;
+}
 </style>
